d338a8600d0d72db1254253fe8f16bf471c00e00
Thibault Santonja <thibault.santonja@gmail.com>
2022-02-06 17:29:46 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Python_Quick_Projects',
+    version='1.0',
+    packages=['snake', 'logger', 'astar_pathfinding'],
+    url='https://github.com/Thibault-Santonja/Python_Quick_Projects',
+    license='MIT',
+    author='Thibault Santonja',
+    author_email='thibault.santonja@gmail.com',
+    description='A lot of micro projects and tests in Python'
+)
