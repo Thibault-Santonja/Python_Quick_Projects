d1f1e34b117ef6bc6f9747ae1ee5684a11b7c20f
Thibault Santonja <thibault.santonja@gmail.com>
2022-02-06 14:53:43 +0100

    Fix Snake tests

--- snake/test_Snake.py
+++ snake/test_Snake.py
@@ -4,28 +4,54 @@ from snake.Point import Point
 
 
 class TestSnake(TestCase):
+    """
+
+    """
     def test_head(self):
+        """
+
+        @return:
+        """
         snake = Snake(Point(0, 0), 1)
         self.assertEqual(snake.head, Point(0, 0))
 
     def test_lenght(self):
-        snake = Snake(Point(0, 0), 3)
-        self.assertEqual(snake.lenght, 3)
-        snake.eat(2, 2)
+        """
+
+        @return:
+        """
+        snake = Snake(Point(0, 0), 4)
         self.assertEqual(snake.lenght, 4)
+        snake.eat(2, 2)
+        self.assertEqual(snake.lenght, 5)
 
     def test_move(self):
+        """
+
+        @return:
+        """
         snake = Snake(Point(0, 0), 1)
         self.assertEqual(snake.move(1, 1), Point(0, 0))
         self.assertEqual(snake.lenght, 1)
 
     def test_eat(self):
+        """
+
+        @return:
+        """
         snake = Snake(Point(0, 0), 1)
-        self.assertEqual(snake.eat(1, 1), False)
-        self.assertEqual(snake.eat(0, 0), True)
+        self.assertEqual(snake.eat(0, 1), True)
+        self.assertEqual(snake.lenght, 2)
+        self.assertEqual(snake.eat(0, 2), True)
+        self.assertEqual(snake.lenght, 3)
+        self.assertEqual(snake.eat(0, -3), False)
         self.assertEqual(snake.lenght, 3)
 
     def test__update_position(self):
+        """
+
+        @return:
+        """
         snake = Snake(Point(0, 0), 1)
         snake.eat(1, 1)
         self.assertEqual(snake.lenght, 2)
@@ -35,6 +61,10 @@ class TestSnake(TestCase):
         self.assertEqual(snake.lenght, 3)
 
     def test__control_self_eating(self):
+        """
+
+        @return:
+        """
         snake = Snake(Point(0, 0), 1)
         snake.eat(1, 1)
         self.assertEqual(snake._control_self_eating(), False)
