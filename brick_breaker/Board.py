832f7ff6a82663d861df7781b7dff5fd79d8799c
Thibault Santonja <thibault.santonja@gmail.com>
2022-02-19 09:39:14 +0100

    Add screen initialization and draw bick
    - Add Draw() to Brick to draw a brick in PyGame Screen
    - Add init_game() to Board to init the game screen

--- brick_breaker/Board.py
+++ brick_breaker/Board.py
@@ -1,33 +1,73 @@
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
+
+import time
+import pygame
+
 from brick_breaker import Pad, Ball, Brick
 
 
 class Board:
-    height: int = 800
-    width: int = 800
-    color: tuple = (249, 228, 212)
+    _height: int = 600
+    _width: int = 800
+    _color: tuple = (249, 228, 212)
     _bricks: list = None
+    _bricks_columns: int = 8
+    _bricks_layers: int = 5
     _pad: Pad.Pad = None
     _ball: Ball.Ball = None
+    _screen: pygame.Surface = None
+    _score_font: pygame.font.Font = None
 
     def __init__(self):
         pad_size = 80
-        self._pad = Pad.Pad(x=(self.width + pad_size)//2, y=self.height//20, size=pad_size)
-        self._ball = Ball.Ball(x=(self.width + pad_size)//2, y=self.height//20)
-        self._init_bricks(8, 3)
+        self._pad = Pad.Pad(x=(self._width + pad_size) // 2, y=self._height // 20, size=pad_size)
+        self._ball = Ball.Ball(x=(self._width + pad_size) // 2, y=self._height // 20)
 
-    def _init_bricks(self, columns: int, layers: int):
+    def __del__(self):
+        pygame.quit()
+
+    def _init_screen(self) -> None:
+        """
+        Initialize the PyGame screen.
+        """
+        pygame.init()
+        self._screen = pygame.display.set_mode((self._width, self._height))
+        pygame.display.set_caption('Brick Breaker Game')
+        self._screen.fill(self._color)
+        self._score_font = pygame.font.SysFont('dejavusans', 32)
+        pygame.display.update()
+
+    def _init_bricks(self, screen_layer_proportion: float = .40, block_gap: int = 2):
         """
 
+        @type screen_layer_proportion: float
+        @param screen_layer_proportion: percentage of screen covered by bricks. (By default 40 %)
+        @type block_gap: int
+        @param block_gap:
         @return:
         """
         self._bricks = []
 
-        for layer in range(layers):
-            for brick in range(columns):
-                self._bricks.append(Brick.Brick(x=self.width//columns * columns + 5,
-                                                y=self.height//layers * layer + 5,
-                                                size_x=self.width//columns - 10,
-                                                size_y=self.height//20))
+        for layer in range(self._bricks_layers):
+            for col in range(self._bricks_columns):
+                brick = Brick.Brick(x=self._width // self._bricks_columns * col + block_gap,
+                                    y=int(self._height / (
+                                            self._bricks_layers * 1/screen_layer_proportion) * layer + block_gap),
+                                    size_x=self._width // self._bricks_columns - block_gap * 2,
+                                    size_y=int(self._height / (
+                                                self._bricks_layers * 1/screen_layer_proportion) - block_gap * 2))
+                brick.draw(pygame, self._screen)
+                self._bricks.append(brick)
+
+        pygame.display.update()
+
+    def _init_game(self):
+        # Init screen and draw it
+        self._init_screen()
+        self._init_bricks()
 
     def launch(self):
-        pass
+        self._init_game()
+
+        time.sleep(10)
--- brick_breaker/Brick.py
+++ brick_breaker/Brick.py
@@ -1,12 +1,15 @@
 class Brick:
-    x: int = None
-    y: int = None
-    size_x: int = None
-    size_y: int = None
-    color: tuple = (214, 125, 62, .9)
+    _x: int = None
+    _y: int = None
+    _size_x: int = None
+    _size_y: int = None
+    _color: tuple = (214, 125, 62, .9)
 
     def __init__(self, x: int, y: int, size_x: int, size_y: int,):
-        self.x = x
-        self.y = y
-        self.size_x = size_x
-        self.size_y = size_y
+        self._x = x
+        self._y = y
+        self._size_x = size_x
+        self._size_y = size_y
+
+    def draw(self, pygame, screen):
+        pygame.draw.rect(screen, self._color, [self._x, self._y, self._size_x, self._size_y])
