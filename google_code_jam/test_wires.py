0127ca6b1a54e9c694cb249776b3eedaaf46c3f2
Thibault Santonja <thibault.santonja@gmail.com>
2022-03-02 17:16:18 +0100

    Clean code & Documentation
    - Add docstrings for functions in wire.py
    - Add missing types
    - Add a performance test

--- google_code_jam/test_wires.py
+++ google_code_jam/test_wires.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-
+import random
 
 from google_code_jam import wires
 
@@ -52,6 +52,16 @@ class Test(TestCase):
         self.assertEqual(wires.calculate_intersections([(1, 0), (2, 1), (-3, -2)]), 0)
         self.assertEqual(wires.calculate_intersections([(-4, 4), (-2, -2), (3, 3)]), 2)
         self.assertEqual(wires.calculate_intersections([(4, -4), (-2, -2), (3, 3)]), 2)
+
+    def test_calculate_intersections_performances(self):
+        # TODO : to be tested with 1000 + a timeout to test performances
+        inputs = list(range(100))
+        outputs = list(range(100))
+
+        random.shuffle(inputs)
+        random.shuffle(outputs)
+
+        print(wires.calculate_intersections(list(zip(inputs, outputs))))
 
     def test_find_line_intersection_point(self):
         self.assertEqual(wires.find_line_intersection_point(
--- google_code_jam/wires.py
+++ google_code_jam/wires.py
@@ -8,17 +8,17 @@ def calculate_intersections(input_links: List[Tuple[int, int]]) -> int:
     @return: Number of intersections between two wires
     """
     # Copy link to keep input data immutability
-    links = input_links.copy()
+    links: List[Tuple[int, int]] = input_links.copy()
 
     # Sort wires
     links.sort()
 
     # Algorithm
-    intersections = []
+    intersections: List[Tuple[float, float]] = []
     for idx, link in enumerate(links):
         for link_ in links[idx:]:
             if link_[0] > link[0] and link_[1] < link[1] or link_[0] < link[0] and link_[1] > link[1]:
-                intersect = find_line_intersection_point(
+                intersect: Tuple[float, float] = find_line_intersection_point(
                     (0, link[0]),
                     (1, link[1]),
                     (0, link_[0]),
@@ -30,11 +30,27 @@ def calculate_intersections(input_links: List[Tuple[int, int]]) -> int:
     return len(intersections)
 
 
-def det(a, b):
+def det(a: Tuple[float, float], b: Tuple[int, int]) -> float:
+    """
+    Calculate determinant of a vector
+
+    @param a: first point
+    @param b: final point
+    @return: determinant
+    """
     return a[0] * b[1] - a[1] * b[0]
 
 
-def find_line_intersection_point(p1, p2, p3, p4):
+def find_line_intersection_point(p1: Tuple[int, int], p2: Tuple[int, int],
+                                 p3: Tuple[int, int], p4: Tuple[int, int]) -> Tuple[float, float]:
+    """
+
+    @param p1: first point of vector 1
+    @param p2: final point of vector 1
+    @param p3: first point of vector 2
+    @param p4: final point of vector 2
+    @return: intersection position
+    """
     xdiff = (p1[0] - p2[0], p3[0] - p4[0])
     ydiff = (p1[1] - p2[1], p3[1] - p4[1])
 
