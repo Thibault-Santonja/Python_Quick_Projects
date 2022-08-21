9e36326523113f909e587292f39129e9053c7413
Thibault Santonja <thibault.santonja@gmail.com>
2022-08-21 19:51:56 +0200

    Update tests for raw_sorting && gph

--- raw_photo_sorter/test_raw_sorter.py
+++ raw_photo_sorter/test_raw_sorter.py
@@ -1,4 +1,6 @@
 from unittest import TestCase
+import io
+import os
 
 from raw_photo_sorter.raw_sorter import *
 
@@ -22,3 +24,20 @@ class Test(TestCase):
             "raw": ["IMG_0000.cr2", "IMG_1234.cr2"]
         }
         self.assertEqual(get_raws_and_jpg(file_list), raws_and_jpg_list)
+
+    def test_get_files(self):
+        res = get_files(".")
+        self.assertTrue('README.md' in res)
+
+    def test_sort_raws_in_folders_failed(self):
+        captured_output = io.StringIO() 
+        sys.stdout = captured_output
+        sort_raws_in_folders(['random_path/'])
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue(), "\nINFO: analyse random_path/\nERROR: Path not found: random_path/\n")
+
+    def test_sort_raws_in_folders(self):
+        self.assertEqual(sort_raws_in_folders(['raw_photo_sorter/']), None)
+        self.assertEqual(os.rmdir('raw_photo_sorter/unused_raw/'), None)
+        with self.assertRaises(FileNotFoundError):
+            os.rmdir('raw_photo_sorter/unused_raw/')
